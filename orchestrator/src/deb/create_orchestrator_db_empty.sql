e45e2f306a351abedc301118bdbaf3584b56b490
Thomas Pedersen <tp@cs.aau.dk>
2018-07-05 10:55:31 +0200

    Debian package generation for ServiceRegistry_SQL, Authorization, Gateway, EventHandler, Gatekeeper, Orchestrator done

--- orchestrator/src/deb/create_orchestrator_db_empty.sql
+++ orchestrator/src/deb/create_orchestrator_db_empty.sql
@@ -2,7 +2,7 @@ CREATE DATABASE  IF NOT EXISTS `arrowhead` /*!40100 DEFAULT CHARACTER SET utf8 *
 USE `arrowhead`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
--- Host: localhost    Database: authorization
+-- Host: localhost    Database: orchestrator
 -- ------------------------------------------------------
 -- Server version	5.7.21-log
 
@@ -18,44 +18,48 @@ USE `arrowhead`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `inter_cloud_authorization`
+-- Table structure for table `orchestration_store`
 --
 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE  IF NOT EXISTS `inter_cloud_authorization` (
+CREATE TABLE  IF NOT EXISTS `orchestration_store` (
   `id` int(11) NOT NULL,
-  `consumer_cloud_id` int(11) DEFAULT NULL,
-  `arrowhead_service_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `UKj4pymxepq7mf82wx7f8e4hd9b` (`consumer_cloud_id`,`arrowhead_service_id`),
-  KEY `FKsh4gbm0vs76weoq1lti6awtwf` (`arrowhead_service_id`),
-  CONSTRAINT `FKsh4gbm0vs76weoq1lti6awtwf` FOREIGN KEY (`arrowhead_service_id`) REFERENCES `arrowhead_service` (`id`),
-  CONSTRAINT `FKsw50x8tjybx1jjrkj6aamxt8c` FOREIGN KEY (`consumer_cloud_id`) REFERENCES `arrowhead_cloud` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `intra_cloud_authorization`
---
-
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE  IF NOT EXISTS `intra_cloud_authorization` (
-  `id` int(11) NOT NULL,
+  `is_default` char(1) DEFAULT NULL,
+  `instruction` varchar(255) DEFAULT NULL,
+  `last_updated` datetime DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `priority` int(11) DEFAULT NULL,
   `consumer_system_id` int(11) DEFAULT NULL,
+  `provider_cloud_id` int(11) DEFAULT NULL,
   `provider_system_id` int(11) DEFAULT NULL,
   `arrowhead_service_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `UK4ie5ps7a6w40iqdte0u53mw1u` (`consumer_system_id`,`provider_system_id`,`arrowhead_service_id`),
-  KEY `FKt01tq84ypy16yfpt2q9v7qn2b` (`provider_system_id`),
-  KEY `FK1nx371ky16pl2rl0f4hk3puk4` (`arrowhead_service_id`),
-  CONSTRAINT `FK1nx371ky16pl2rl0f4hk3puk4` FOREIGN KEY (`arrowhead_service_id`) REFERENCES `arrowhead_service` (`id`),
-  CONSTRAINT `FK58r9imuaq3dy3o96w5xcxkemh` FOREIGN KEY (`consumer_system_id`) REFERENCES `arrowhead_system` (`id`),
-  CONSTRAINT `FKt01tq84ypy16yfpt2q9v7qn2b` FOREIGN KEY (`provider_system_id`) REFERENCES `arrowhead_system` (`id`)
+  UNIQUE KEY `UK328vwkn9l8phjq4j276wb13w9` (`arrowhead_service_id`,`consumer_system_id`,`priority`,`is_default`),
+  KEY `FKg9jtg1go2yety7s6qimnbqdtc` (`consumer_system_id`),
+  KEY `FK4as8nlx9s4a6a9r6y4oswj5do` (`provider_cloud_id`),
+  KEY `FK1a9yusgvqs0jrna2y8cgdeusb` (`provider_system_id`),
+  CONSTRAINT `FK1a9yusgvqs0jrna2y8cgdeusb` FOREIGN KEY (`provider_system_id`) REFERENCES `arrowhead_system` (`id`),
+  CONSTRAINT `FK4as8nlx9s4a6a9r6y4oswj5do` FOREIGN KEY (`provider_cloud_id`) REFERENCES `arrowhead_cloud` (`id`),
+  CONSTRAINT `FKg9jtg1go2yety7s6qimnbqdtc` FOREIGN KEY (`consumer_system_id`) REFERENCES `arrowhead_system` (`id`),
+  CONSTRAINT `FKnjr4mytp6bipwyc9sv9y1ip51` FOREIGN KEY (`arrowhead_service_id`) REFERENCES `arrowhead_service` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `orchestration_store_attributes`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE  IF NOT EXISTS `orchestration_store_attributes` (
+  `store_entry_id` int(11) NOT NULL,
+  `attribute_value` varchar(2047) DEFAULT NULL,
+  `attribute_key` varchar(255) NOT NULL,
+  PRIMARY KEY (`store_entry_id`,`attribute_key`),
+  CONSTRAINT `FKrtqe93seoude4elrqmk1qdowj` FOREIGN KEY (`store_entry_id`) REFERENCES `orchestration_store` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -66,4 +70,4 @@ CREATE TABLE  IF NOT EXISTS `intra_cloud_authorization` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-05-24 13:43:43
+-- Dump completed on 2018-05-24 13:44:51
