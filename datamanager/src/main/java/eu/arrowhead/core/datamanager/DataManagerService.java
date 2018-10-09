d6e40f1f854464fb4b721a48a85c871e61bba760
Jens Eliasson <jens.eliasson@gmail.com>
2018-10-09 09:31:26 +0200

    initial code for database support

--- datamanager/src/main/java/eu/arrowhead/core/datamanager/DataManagerService.java
+++ datamanager/src/main/java/eu/arrowhead/core/datamanager/DataManagerService.java
@@ -25,13 +25,64 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Stream;
 import org.apache.log4j.Logger;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Restrictions;
 
-final class DataManagerService {
-
+public class DataManagerService {
   private static final Logger log = Logger.getLogger(DataManagerResource.class.getName());
   private static final DatabaseManager dm = DatabaseManager.getInstance();
+  private static SessionFactory factory;
 
   private static List<String> endpoints = new ArrayList<>();
+
+  public DataManagerService() throws Exception {
+    try {
+      factory = new Configuration().configure().buildSessionFactory();
+    } catch (Throwable ex) {
+      System.err.println("Failed to create sessionFactory object." + ex);
+      throw new ExceptionInInitializerError(ex);
+    }
+  }
+
+
+  /**
+   *
+   *
+   */
+  static int lookupEndpoint(String name) {
+    Session session = factory.openSession();
+    Transaction tx = null;
+
+    try {
+      tx = session.beginTransaction();
+      Criteria cr = session.createCriteria(ArrowheadSystem.class);
+
+      if (name != null) {
+	cr.add(Restrictions.like("name", "%" + name + "%"));
+      }
+
+      List<ArrowheadSystem> systems = cr.list();
+      for (Iterator iterator = systems.iterator(); iterator.hasNext(); ) {
+	ArrowheadSystem ahsys = (ArrowheadSystem) iterator.next();
+      
+      }
+
+    } catch (HibernateException e) {
+      if (tx != null) {
+	tx.rollback();
+      }
+
+      e.printStackTrace();
+    } 
+
+    return -1; //not found
+  }
+
 
   static boolean createEndpoint(String name) {
     Iterator<String> epi = endpoints.iterator();
