373bdce4d7faaac0c271208282c9a3a84028c63d
Jens Eliasson <jens.eliasson@gmail.com>
2018-10-09 09:32:20 +0200

    Class for storing information about DM clients

---
+++ datamanager/src/main/java/eu/arrowhead/core/datamanager/ArrowheadSystem.java
@@ -0,0 +1,22 @@
+/*
+ *  Copyright (c) 2018 Jens Eliasson, Luleå University of Technology
+ *
+ *  This work is part of the Productive 4.0 innovation project, which receives grants from the
+ *  European Commissions H2020 research and innovation programme, ECSEL Joint Undertaking
+ *  (project no. 737459), the free state of Saxony, the German Federal Ministry of Education and
+ *  national funding authorities from involved countries.
+ */
+
+package eu.arrowhead.core.datamanager;
+
+public class ArrowheadSystem {
+  String name;
+  int id;
+
+ 
+  public ArrowheadSystem(String name, int id) {
+    this.name = new String(name);
+    this.id = id; 
+  }
+
+}
