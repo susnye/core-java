dc2c234668c78f41ee29569ba288f2f0b0bce584
Mario Zsilak <17300435+mzsilak@users.noreply.github.com>
2018-10-11 11:52:41 +0200

    Fixed compilation issue and removed swagger from Main class

--- deviceregistry/src/main/java/eu/arrowhead/core/deviceregistry/DeviceRegistryMain.java
+++ deviceregistry/src/main/java/eu/arrowhead/core/deviceregistry/DeviceRegistryMain.java
@@ -9,56 +9,21 @@ package eu.arrowhead.core.deviceregistry;
 
 import eu.arrowhead.common.ArrowheadMain;
 import eu.arrowhead.common.misc.CoreSystem;
-import eu.arrowhead.core.deviceregistry.filter.DeviceRegACF;
-import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
-
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
-
-import org.glassfish.grizzly.http.server.CLStaticHttpHandler;
-import org.glassfish.grizzly.http.server.HttpHandler;
-import org.glassfish.grizzly.http.server.HttpServer;
 
 public class DeviceRegistryMain extends ArrowheadMain {
 
   private DeviceRegistryMain(String[] args) {
 
-    Set<Class<?>> classes = new HashSet<>(Arrays.asList(
-    																	DeviceRegistryResource.class, 
-    																	DeviceRegistryApi.class,
-    																	DeviceRegACF.class,
-    																	OpenApiResource.class,
-    																	DeviceRegistryMain.class));
     String[] packages = {
     		"eu.arrowhead.common.exception", 
     		"eu.arrowhead.common.json", 
     		"eu.arrowhead.common.filter", 
 				"eu.arrowhead.core.systemregistry"};
-    init(CoreSystem.DEVICEREGISTRY, args, classes, packages);
+    init(CoreSystem.DEVICE_REGISTRY, args, null, packages);
 
 
     listenForInput();
   }
-	protected void adaptServer(HttpServer server) {
-		final HttpHandler httpHandler = new CLStaticHttpHandler(HttpServer.class.getClassLoader(), "/swagger/");
-		server.getServerConfiguration().addHttpHandler(httpHandler, "/swagger");
-	}
-
-	@GET
-	@Path("/swagger")
-	public Response swagger(@Context UriInfo uriInfo) throws URISyntaxException {
-		final URI uri = new URI(uriInfo.getBaseUri().toString() + "/swagger/");
-		return Response.status(Response.Status.TEMPORARY_REDIRECT).contentLocation(uri).build();
-	}
 
   public static void main(String[] args) {
     new DeviceRegistryMain(args);
