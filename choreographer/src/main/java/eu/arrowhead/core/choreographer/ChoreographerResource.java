c52fb771070daa9caf88d2cf27d1e2c8551a060e
DESKTOP-EECSIA8\szabo <szaboo.kris@gmail.com>
2019-04-15 10:30:51 +0200

    Pom.xml fix, added Choreographer to run configurations and added new_sql generate file with fixed sql database (backend does not support new database structure yet).

--- choreographer/src/main/java/eu/arrowhead/core/choreographer/ChoreographerResource.java
+++ choreographer/src/main/java/eu/arrowhead/core/choreographer/ChoreographerResource.java
@@ -7,16 +7,25 @@
 
 package eu.arrowhead.core.choreographer;
 
+import eu.arrowhead.common.messages.ServiceRequestForm;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.apache.log4j.Logger;
 
+
+/**
+ * This is the REST resource for the Workflow Choreographer Core System.
+ */
 @Path("choreographer")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class ChoreographerResource {
+
+  private static final Logger log = Logger.getLogger(ChoreographerResource.class.getName());
 
   @GET
   @Produces(MediaType.TEXT_PLAIN)
