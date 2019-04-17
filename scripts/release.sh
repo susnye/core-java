db28ff2f1ee924514e44bef817dd8976b100f11e
Thomas Pedersen <tp@cs.aau.dk>
2019-04-17 18:06:01 +0200

    Added dependency notice to help me not forget!

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,3 +3,9 @@
 mvn install:install-file -DgroupId=eu.arrowhead -DartifactId=arrowhead-common -Dversion=4.1.3-SNAPSHOT \
     -Dfile=$(dirname "$0")/../common/target/arrowhead-common-4.1.3-SNAPSHOT.jar -Dpackaging=jar \
     -DgeneratePom=true -DlocalRepositoryPath=$(pwd) -DcreateChecksum=true
+
+echo
+echo "-------------------------------------------------------"
+echo " REMEMBER TO ADD DEPENDENCIES TO POM FILE MANUALLY !!! "
+echo "-------------------------------------------------------"
+echo
