9715b8421d83c75ae2a183006755cbc0f07e7e37
Gabor Singler <gabor.singler@gmail.com>
2019-07-10 09:09:33 +0200

    Intra cloud auth rule insertion when no auth rule exists fixed

--- scripts/dbins_intra_cloud_auth_rule.sh
+++ scripts/dbins_intra_cloud_auth_rule.sh
@@ -12,7 +12,14 @@ CONSUMER=$1
 PROVIDER=$2
 SERVICE=$3
 QRYMAXICAID="select max(id)+1 from intra_cloud_authorization;"
+
 ICAID=$(mysql -u root -p -Bse "$USEDB$QRYMAXICAID")
+
+if [ $ICAID == "NULL" ]
+	then
+	ICAID=1
+fi
+
 INSCMD="insert into intra_cloud_authorization(consumer_system_id, provider_system_id, arrowhead_service_id, id) values($CONSUMER, $PROVIDER, $SERVICE, $ICAID);"
 SQLCMD=$USEDB$INSCMD
 echo $SQLCMD
